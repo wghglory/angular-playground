5129e9b39cd7a056f53224479c551672a6b6e3cb
Derek Wang <guanghuiw@vmware.com>
2023-04-22 20:27:53 +0800

    feat: add clarity and tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClarityModule } from '@clr/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +10,13 @@ import { NotFoundComponent } from './shared/ui/not-found/not-found.component';
 
 @NgModule({
   declarations: [AppComponent, ConditionalContentProjectionComponent],
-  imports: [BrowserModule, AppRoutingModule, NotFoundComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ClarityModule,
+    NotFoundComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
